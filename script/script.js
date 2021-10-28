ce47556ae9e21400ddcd2b89574fb5fb3c8b9fb1
Vyacheslav Kobetskoy <slava.19.07@yandex.ru>
2021-10-28 15:24:11 +0300

    feat:popup

---
+++ script/script.js
@@ -0,0 +1,26 @@
+const popup = document.querySelector(".popup");
+const closeBtn = document.querySelector(".popup__close-btn");
+const еditBtn = document.querySelector(".profile__еdit-btn");
+
+const profileName = document.querySelector(".profile__name");
+const profileStatus = document.querySelector(".profile__status");
+const popupName = document.querySelector(".popup__name");
+const popupStatus = document.querySelector(".popup__status");
+const formElement = document.querySelector(".popup__container");
+
+еditBtn.addEventListener("click", () => {
+  popup.classList.add("popup_opened");
+  popupName.value = profileName.textContent;
+  popupStatus.value = profileStatus.textContent;
+});
+
+closeBtn.addEventListener("click", () => {
+  popup.classList.remove("popup_opened");
+});
+
+formElement.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  profileName.textContent = popupName.value;
+  profileStatus.textContent = popupStatus.value;
+  popup.classList.remove("popup_opened");
+});
