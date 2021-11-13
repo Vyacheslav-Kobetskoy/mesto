1d8b672a69bb25da7e953e20211683b14a04cdfb
Vyacheslav Kobetskoy <slava.19.07@yandex.ru>
2021-11-13 18:58:14 +0300

    feat: gallery list

--- script/script.js
+++ script/script.js
@@ -28,3 +28,49 @@ function saveChanges(evt) {
 editBtn.addEventListener("click", openPopup);
 closeBtn.addEventListener("click", closePopup);
 formElement.addEventListener("submit", saveChanges);
+
+const initialCards = [
+  {
+    name: "Архыз",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+  },
+  {
+    name: "Челябинская область",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+  },
+  {
+    name: "Иваново",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+  },
+  {
+    name: "Камчатка",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+  },
+  {
+    name: "Холмогорский район",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+  },
+  {
+    name: "Байкал",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  },
+];
+const gallery = document.querySelector(".gallery");
+const template = document.querySelector(".template");
+
+const createCard = (item) => {
+  const galleryCard = template.content.querySelector(".gallery__card").cloneNode(true);
+  const galleryPhoto = galleryCard.querySelector(".gallery__photo");
+  const galleryPhotoTitle = galleryCard.querySelector(".gallery__photo-title");
+  galleryPhoto.src = item.link;
+  galleryPhoto.alt = item.name;
+  galleryPhotoTitle.textContent = item.name;
+  return galleryCard;
+};
+
+const result = initialCards.map((item) => {
+  return createCard(item);
+});
+
+gallery.append(...result);
+
